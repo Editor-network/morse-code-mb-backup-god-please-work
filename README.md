8fd73ae9e17eadf027f2b5bdbd8935028ff5660e
Editor-network <123669690+Editor-network@users.noreply.github.com>
2024-11-24 19:04:02 +0000

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,50 @@
+
+> Open this page at [https://editor-network.github.io/morse-code-backup/](https://editor-network.github.io/morse-code-backup/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/editor-network/morse-code-backup** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/editor-network/morse-code-backup** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+
+
+
+> Open this page at [https://editor-network.github.io/morse-code-mb-backup-god-please-work/](https://editor-network.github.io/morse-code-mb-backup-god-please-work/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/editor-network/morse-code-mb-backup-god-please-work** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/editor-network/morse-code-mb-backup-god-please-work** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,169 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+let string = ""
+let string_trigger = 0
+let _1st_2nd_3rd_4th_swap_trigger = 0
+let _3rd = 0
+let _2nd = 0
+let _1st = 0
+let sequance = ""
+let _4th = 6
+let a = 1233
+let b = 2111
+let c = 2121
+let d = 2113
+let e = 1333
+let f = 1121
+let g = 2213
+let h = 1111
+let i = 1133
+let j = 1222
+let k = 2123
+let l = 1211
+let m = 2233
+let n = 2133
+let o = 2223
+let p = 1221
+let q = 2212
+let r = 1213
+let s = 1113
+let t = 2333
+let u = 2213
+let v = 1113
+let w = 1223
+let x = 2112
+let y = 2122
+let z = 2211
+basic.forever(function () {
+    while (_4th < 5) {
+        sequance = "" + _1st + _2nd + _3rd + _4th
+        _1st = 0
+        _2nd = 0
+        _3rd = 0
+        _4th = 0
+        _1st_2nd_3rd_4th_swap_trigger = 0
+        string_trigger = 1
+    }
+})
+basic.forever(function () {
+    while (1 == string_trigger) {
+        if (a == sequance) {
+            string = "" + string + "a"
+        } else {
+            if (a == sequance) {
+                string = "" + string + "a"
+            } else {
+                if (a == sequance) {
+                    string = "" + string + "a"
+                } else {
+                    if (a == sequance) {
+                        string = "" + string + "a"
+                    } else {
+                        if (a == sequance) {
+                            string = "" + string + "a"
+                        } else {
+                            if (a == sequance) {
+                                string = "" + string + "a"
+                            } else {
+                                if (a == sequance) {
+                                    string = "" + string + "a"
+                                } else {
+                                    if (a == sequance) {
+                                        string = "" + string + "a"
+                                    } else {
+                                        if (a == sequance) {
+                                            string = "" + string + "a"
+                                        } else {
+                                            if (a == sequance) {
+                                                string = "" + string + "a"
+                                            } else {
+                                                if (a == sequance) {
+                                                    string = "" + string + "a"
+                                                } else {
+                                                    if (a == sequance) {
+                                                        string = "" + string + "a"
+                                                    } else {
+                                                        if (a == sequance) {
+                                                            string = "" + string + "a"
+                                                        } else {
+                                                            if (a == sequance) {
+                                                                string = "" + string + "a"
+                                                            } else {
+                                                                if (a == sequance) {
+                                                                    string = "" + string + "a"
+                                                                } else {
+                                                                    if (a == sequance) {
+                                                                        string = "" + string + "a"
+                                                                    } else {
+                                                                        if (a == sequance) {
+                                                                            string = "" + string + "a"
+                                                                        } else {
+                                                                            if (a == sequance) {
+                                                                                string = "" + string + "a"
+                                                                            } else {
+                                                                                if (a == sequance) {
+                                                                                    string = "" + string + "a"
+                                                                                } else {
+                                                                                    if (a == sequance) {
+                                                                                        string = "" + string + "a"
+                                                                                    } else {
+                                                                                        if (a == sequance) {
+                                                                                            string = "" + string + "a"
+                                                                                        } else {
+                                                                                            if (a == sequance) {
+                                                                                                string = "" + string + "a"
+                                                                                            } else {
+                                                                                                if (a == sequance) {
+                                                                                                    string = "" + string + "a"
+                                                                                                } else {
+                                                                                                    if (a == sequance) {
+                                                                                                        string = "" + string + "a"
+                                                                                                    } else {
+                                                                                                        if (a == sequance) {
+                                                                                                            string = "" + string + "a"
+                                                                                                        } else {
+                                                                                                            if (a == sequance) {
+                                                                                                                string = "" + string + "a"
+                                                                                                            } else {
+                                                                                                                if (a == sequance) {
+                                                                                                                    string = "" + string + "a"
+                                                                                                                } else {
+                                                                                                                    if (a == sequance) {
+                                                                                                                        string = "" + string + "a"
+                                                                                                                    } else {
+                                                                                                                        if (a == sequance) {
+                                                                                                                            string = "" + string + "a"
+                                                                                                                        } else {
+                                                                                                                        	
+                                                                                                                        }
+                                                                                                                    }
+                                                                                                                }
+                                                                                                            }
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+})
